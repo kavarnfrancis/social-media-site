40b069d5cb265abc724e7f29465b76c40b2fcfb7
frankm2 <frankm2@D201-71849>
2025-10-08 15:47:39 +1300

    Add blog to nav bar

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,8 @@
+<h2>My Blog Contents Page</h2>
+
+<nav>
+	<a href="/blog/bananas"></a>
+    <a href="/bog/web-is-cool"></a>
+    <a href="/blog/eight"></a>
+    <a href="/blog/ideas-are-hard"></a>
+</nav>
