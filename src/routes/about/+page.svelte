a28ad87323db4b3e77689f38ee410ca2d1157704
frankm2 <frankm2@D201-71849>
2025-10-08 15:22:51 +1300

    Initialize about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    
+</script>
+
+<h2>About this site</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat consequatur itaque magni minima iure? Magni officiis facere eius ducimus in quam, itaque ratione. Provident ad sint temporibus? Velit, exercitationem quam.</p>
+
