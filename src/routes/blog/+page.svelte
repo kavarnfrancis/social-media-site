f57c1ae4bdb15552ae1b2542c343d5c8f2fb38aa
frankm2 <frankm2@D201-71849>
2025-10-08 15:50:47 +1300

    Use a dynamic route for blog posts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,8 +1,15 @@
 <h2>My Blog Contents Page</h2>
 
 <nav>
-	<a href="/blog/bananas"></a>
-    <a href="/bog/web-is-cool"></a>
-    <a href="/blog/eight"></a>
-    <a href="/blog/ideas-are-hard"></a>
+	<a href="/blog/bananas">Bananas</a>
+    <a href="/bog/web-is-cool">Web is Cool</a>
+    <a href="/blog/eight">Eight</a>
+    <a href="/blog/ideas-are-hard">Ideas are Hard</a>
 </nav>
+
+<style>
+    nav {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
