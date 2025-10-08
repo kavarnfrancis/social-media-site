fe4f9e73286aa380956e64e775cb14263bbbed9a
frankm2 <frankm2@D201-71849>
2025-10-08 15:58:42 +1300

    dynamic blog posts create a page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,8 +1,18 @@
+<script>
+    let {data} = $props();
+</script>
+
 <h2>My Blog Contents Page</h2>
 
 <nav>
+    {#each data.posts as post}
+        <a href="/blog/{post.id}">{post.title}</a>
+    {/each}
+</nav>
+
+<nav>
 	<a href="/blog/bananas">Bananas</a>
-    <a href="/bog/web-is-cool">Web is Cool</a>
+    <a href="/blog/web-is-cool">Web is Cool</a>
     <a href="/blog/eight">Eight</a>
     <a href="/blog/ideas-are-hard">Ideas are Hard</a>
 </nav>
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,2 @@
+<h2>Blog Article</h2>
+<p>Coming soon...</p>
